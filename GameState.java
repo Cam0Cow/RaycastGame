0329d81c2850ed42bea2e2e6857496150ca80db5
Cam0Cow <roie.roiegal@gmail.com>
2019-02-05 23:23:29 -0600

    Layed out basic GameState class

---
+++ GameState.java
@@ -0,0 +1,24 @@
+
+
+/**
+ * Represents the state of the game at any given time
+ */
+public class GameState {
+    
+    // This will contain the screen data, enemies, stats, etc.
+    // It will be updated as new features are added
+    
+    private static GameState state;
+    
+    /**
+     * Constructs a new game state if necessary  
+    */
+    private GameState() {
+        // Initialization here
+    }
+    
+    public static GameState getGameState() {
+        if (state == null) state = new GameState();
+        return state;
+    }
+}
